2d08abb1a087d9dc97e211d5e2f26121d8ed5d16
Nicholas Bennet <bennet08121996nicholas@gmail.com>
2020-04-19 13:16:46 -0400

    Updates

---
+++ precache-manifest.603a2edd3110fea08776ad86ab17292c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a521584083a1ca38652f166034c9b00c",
+    "url": "/random-recipe/index.html"
+  },
+  {
+    "revision": "669375b5cd3fb968f43a",
+    "url": "/random-recipe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4a3f8ed6728c06150868",
+    "url": "/random-recipe/static/js/2.8397e3de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-recipe/static/js/2.8397e3de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669375b5cd3fb968f43a",
+    "url": "/random-recipe/static/js/main.0fbd1c93.chunk.js"
+  },
+  {
+    "revision": "a36d3e8d2e2f27432f70",
+    "url": "/random-recipe/static/js/runtime-main.700ce62f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-recipe/static/media/logo.5d5d9eef.svg"
+  }
+]);
